a05655951e10bac821107de2892cc8c94a3ca9b5
Torsten Knoefel <torsten.knoefel@hotmail.de>
2017-04-27 00:43:51 +0100

    Add project files.

---
+++ WorkingWithVisualStudio.Tests/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WorkingWithVisualStudio.Controllers;
+using WorkingWithVisualStudio.Models;
+using Xunit;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexActionModelIsComplete() {
+            // Arrange
+            var controller = new HomeController();
+
+            //Act
+            var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            // Assert
+            Assert.Equal(SimpleRepository.SharedRepository.Products, model, Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name && p1.Price == p2.Price));
+        }
+    }
+}
